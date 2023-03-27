74b97e30d2f9ea9a3e276e534db3456db1bcaa66
Ed Slavin <105741816+erslavin@users.noreply.github.com>
2023-03-27 06:23:33 -0400

    Om 51135 usd schema tutorial upgrade (#2)
    * Initial open source release
    
    - Removed dependencies on repo tooling
    - Added support for cmake
    - Integrated kit-extension based on kit-extension github template
    
    * Initial open source release
    
    - Fixed issue in directory casing for kit
    - Fixed small issues in codeless schema file
    
    * Initial open source release
    
    - Adapted build to use new default module structure
    - Bumped repo_usd version
    - Fixed casing in directory for extension init
    
    * Initial open source release
    
    - Updated executable bits on relevant files
    - Updated linux build script to include build option
    - Updated repo.toml to include install root for schemas
    
    * Initial open source release
    
    - Fixed issue with copying files on Windows
    
    * Initial open source release
    
    - Updated README to reflect options available in repo.toml
    
    * Initial open source release
    
    - Cleaned up Linux directory structure
    - Aligned build files for Windows and Linux
    - Added build and prerequisite information to README
    
    * Initial open source release
    
    - Made clear in README that curl only needed for linux
    
    * Initial open source release
    
    - Bumped python version to 3.7.16
    
    * Om 51135 usd schema tutorial upgrade tweaks (#3)
    
    * clarify library_prefix vs libraryPrefix
    
    * make build.sh also remove _build and _repo like build.bat
    
    * clean up --help flag for build.bat / build.sh
    
    - order flags in same order steps are executed
    - remove printed " from .bat
    - add missing help for --build and --help flags
    - try to emphasize that --build is more USD-focused, and --stage is
      more kit-focused
    - fix "build.bat" in help of build.sh
    
    * add a helpful error message if build.bat/.sh invoked with no actions
    
    also exit immediately if --help passed, regardless of other flags, like
    most command line parsers behave
    
    * clarify a comment that I found confusing in build.bat/.sh about premake vs cmake
    
    * fix out-of-date comment in README.md about generate_root_makefile being turned off
    
    * make --clean also remove src/usd-plugins/schema/*/generated
    
    * Initial open source release
    
    - Bumped version of repo_usd to address issues with Windows drive paths
    
    * Initial open source release
    
    - Fixed issue with linux source copy paths
    
    ---------
    
    Co-authored-by: Paul Molodowitch <elrond79@gmail.com>

---
+++ build.bat
@@ -0,0 +1,179 @@
+:: Copyright 2023 NVIDIA CORPORATION
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+::    http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+
+@echo off
+setlocal enabledelayedexpansion
+
+pushd %~dp0
+
+REM options defining what the script runs
+set GENERATE=false
+set BUILD=false
+set CLEAN=false
+set CONFIGURE=false
+set STAGE=false
+set HELP=false
+set CONFIG=release
+set HELP_EXIT_CODE=0
+
+set DIRECTORIES_TO_CLEAN=_install _build _repo src\usd-plugins\schema\omniExampleSchema\generated src\usd-plugins\schema\omniExampleCodelessSchema\generated
+
+REM default arguments for script - note this script does not actually perform
+REM any build step so that you can integrate the generated code into your build
+REM system - an option can be added here to run your build step (e.g. cmake)
+REM on the generated files
+:parseargs
+if not "%1"=="" (
+    if "%1" == "--clean" (
+        set CLEAN=true
+    )
+    if "%1" == "--generate" (
+        set GENERATE=true
+    )
+    if "%1" == "--build" (
+        set BUILD=true
+    )
+    if "%1" == "--stage" (
+        set STAGE=true
+    )
+    if "%1" == "--configure" (
+        set CONFIGURE=true
+    )
+    if "%1" == "--debug" (
+        set CONFIG=debug
+    )
+    if "%1" == "--help" (
+        set HELP=true
+    )
+    shift
+    goto :parseargs
+)
+
+if not "%CLEAN%" == "true" (
+    if not "%GENERATE%" == "true" (
+        if not "%BUILD%" == "true" (
+            if not "%STAGE%" == "true" (
+                if not "%CONFIGURE%" == "true" (
+                    if not "%HELP%" == "true" (
+                        set HELP=true
+                        set HELP_EXIT_CODE=1
+                        echo No actions selected - specify at least one of:
+                        echo   --clean --generate --build --stage --configure --help
+                        echo.
+                    )
+                )
+            )
+        )
+    )
+)
+
+REM requesting how to run the script
+if "%HELP%" == "true" (
+    echo build.bat [--clean] [--generate] [--build] [--stage] [--configure] [--debug] [--help]
+    echo --clean: Removes the following directories ^(customize as needed^):
+    for %%a in (%DIRECTORIES_TO_CLEAN%) DO (
+        echo       %%a
+    )
+    echo --generate: Perform code generation of schema libraries
+    echo --build: Perform compilation and installation of USD schema libraries
+    echo --stage: Preps the kit-extension by copying it to the _install directory and stages the
+    echo       built USD schema libraries in the appropriate sub-structure
+    echo --configure: Performs a configuration step when using premake after you have built and
+    echo       staged the schema libraries to ensure the plugInfo.json has the right information
+    echo --debug: Performs the steps with a debug configuration instead of release
+    echo       ^(default = release^)
+    echo --help: Display this help message
+    exit %HELP_EXIT_CODE%
+)
+
+REM should we clean the target directory?
+if "%CLEAN%" == "true" (
+    for %%a in (%DIRECTORIES_TO_CLEAN%) DO (
+        if exist "%~dp0%%a/" (
+            rmdir /s /q "%~dp0%%a"
+        )
+    )
+
+    if !errorlevel! neq 0 (goto Error)
+
+    goto Success
+)
+
+REM should we generate the schema code?
+if "%GENERATE%" == "true" (
+
+    REM pull down NVIDIA USD libraries
+    REM NOTE: If you have your own local build, you can comment out these steps
+    call "%~dp0tools\packman\packman.cmd" pull deps/usd-deps.packman.xml -p windows-x86_64 -t config=debug
+    call "%~dp0tools\packman\packman.cmd" pull deps/usd-deps.packman.xml -p windows-x86_64 -t config=release
+
+    if !errorlevel! neq 0 ( goto Error )
+
+    REM generate the schema code and plug-in information
+    REM NOTE: this will pull the NVIDIA repo_usd package to do this work
+
+    call "%~dp0tools\packman\python.bat" bootstrap.py usd
+
+    if !errorlevel! neq 0 ( goto Error )
+)
+
+REM should we build the USD schema?
+
+REM NOTE: Modify this build step if using a build system other than cmake (ie, premake)
+if "%BUILD%" == "true" (
+    REM Below is an example of using CMake to build the generated files
+    REM You may also want to explicitly specify the toolset depending on which
+    REM version of Visual Studio you are using (e.g. -T v141)
+    cmake -B ./_build/cmake
+    cmake --build ./_build/cmake --config=%CONFIG% --target install
+)
+
+REM do we need to stage?
+if "%STAGE%" == "true" (
+    REM Copy the kit-extension into the _install directory
+    REM we can do this directly because it's a python extension, so there's nothing to compile
+    REM but we want to combine it together with the output of the USD schema extension
+    REM Why not build the USD schema extension to the exact structure we are creating here?  
+    REM Because we are illustrating that you can build the C++ schemas and distribute them
+    REM independent of anything kit related.  All the copy is doing is putting everything in 
+    REM one structure that can be referenced as a complete kit extension
+    echo D | xcopy "%~dp0src\kit-extension\exts\omni.example.schema" "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema" /s /Y
+    if not exist "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleSchema" mkdir "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleSchema"
+    echo F | xcopy "%~dp0_install\windows-x86_64\%CONFIG%\omniExampleSchema\OmniExampleSchema\*.*" "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleSchema" /Y
+    echo D | xcopy "%~dp0_install\windows-x86_64\%CONFIG%\omniExampleSchema\include" "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleSchema\include" /s /Y
+    echo D | xcopy "%~dp0_install\windows-x86_64\%CONFIG%\omniExampleSchema\lib" "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleSchema\lib" /s /Y
+    echo D | xcopy "%~dp0_install\windows-x86_64\%CONFIG%\omniExampleSchema\resources" "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleSchema\resources" /s /Y
+    if not exist "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleCodelessSchema" mkdir "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleCodelessSchema"
+    echo D | xcopy "%~dp0_install\windows-x86_64\%CONFIG%\omniExampleCodelessSchema\resources" "%~dp0_install\windows-x86_64\%CONFIG%\omni.example.schema\OmniExampleCodelessSchema\resources" /s /Y
+    
+    if !errorlevel! neq 0 ( goto Error )
+)
+
+REM is this a configure only?
+REM When using premake, the plugInfo.json files
+REM do not get their tokens replaced as premake
+REM does not have this functionality built in like cmake
+if "%CONFIGURE%" == "true" (
+    call "%~dp0tools\packman\python.bat" bootstrap.py usd --configure-plugInfo
+
+    if !errorlevel! neq 0 (goto Error)
+
+    goto Success
+)
+
+:Success
+exit /b 0
+
+:Error
+exit /b !errorlevel!
---
+++ build.sh
@@ -0,0 +1,158 @@
+# Copyright 2023 NVIDIA CORPORATION
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+CWD="$( cd "$( dirname "$0" )" && pwd )"
+
+# default config is release
+CLEAN=false
+BUILD=false
+GENERATE=false
+STAGE=false
+CONFIGURE=false
+HELP=false
+CONFIG=release
+HELP_EXIT_CODE=0
+
+DIRECTORIES_TO_CLEAN=(
+    _install
+    _build
+    _repo
+    src/usd-plugins/schema/omniExampleCodelessSchema/generated
+    src/usd-plugins/schema/omniExampleSchema/generated
+)
+
+while [ $# -gt 0 ]
+do
+    if [[ "$1" == "--clean" ]]
+    then
+        CLEAN=true
+    fi
+    if [[ "$1" == "--generate" ]]
+    then
+        GENERATE=true
+    fi
+    if [[ "$1" == "--build" ]]
+    then
+        BUILD=true
+    fi
+    if [[ "$1" == "--stage" ]]
+    then
+        STAGE=true
+    fi
+    if [[ "$1" == "--configure" ]]
+    then
+        CONFIGURE=true
+    fi
+    if [[ "$1" == "--debug" ]]
+    then
+        CONFIG=debug
+    fi
+    if [[ "$1" == "--help" ]]
+    then
+        HELP=true
+    fi
+    shift
+done
+
+if [[
+        "$CLEAN" != "true"
+        && "$GENERATE" != "true"
+        && "$BUILD" != "true"
+        && "$STAGE" != "true"
+        && "$CONFIGURE" != "true"
+        && "$HELP" != "true"
+    ]]
+then
+    HELP=true
+    HELP_EXIT_CODE=1
+    echo "No actions selected - specify at least one of:"
+    echo "  --clean --generate --build --stage --configure --help"
+    echo ""
+fi
+
+# requesting how to run the script
+if [[ "$HELP" == "true" ]]
+then
+    echo "build.sh [--clean] [--generate] [--build] [--stage] [--configure] [--debug] [--help]"
+    echo "--clean: Removes the following directories (customize as needed):"
+    for dir_to_clean in "${DIRECTORIES_TO_CLEAN[@]}" ; do
+        echo "      $dir_to_clean"
+    done
+    echo "--generate: Perform code generation of schema libraries"
+    echo "--build: Perform compilation and installation of USD schema libraries"
+    echo "--stage: Preps the kit-extension by copying it to the _install directory and stages the"
+    echo "      built USD schema libraries in the appropriate sub-structure"
+    echo "--configure: Performs a configuration step when using premake after you have built and"
+    echo "      staged the schema libraries to ensure the plugInfo.json has the right information"
+    echo "--debug: Performs the steps with a debug configuration instead of release"
+    echo "      (default = release)"
+    echo "--help: Display this help message"
+    exit $HELP_EXIT_CODE
+fi
+
+# do we need to clean?
+if [[ "$CLEAN" == "true" ]]
+then
+    for dir_to_clean in "${DIRECTORIES_TO_CLEAN[@]}" ; do
+        rm -rf "$CWD/$dir_to_clean"
+    done
+fi
+
+# do we need to generate?
+if [[ "$GENERATE" == "true" ]]
+then
+    # pull down NVIDIA USD libraries
+    # NOTE: If you have your own local build, you can comment out these steps
+    $CWD/tools/packman/packman pull deps/usd-deps.packman.xml -p linux-$(arch) -t config=debug
+    $CWD/tools/packman/packman pull deps/usd-deps.packman.xml -p linux-$(arch) -t config=release
+
+    # generate the schema code and plug-in information
+    # NOTE: this will pull the NVIDIA repo_usd package to do this work
+    export CONFIG=$CONFIG
+    $CWD/tools/packman/python.sh bootstrap.py usd
+fi
+
+# do we need to build?
+
+# NOTE: Modify this build step if using a build system other than cmake (ie, premake)
+if [[ "$BUILD" == "true" ]]
+then
+    # Below is an example of using CMake to build the generated files
+    cmake -B ./_build/cmake -DCMAKE_BUILD_TYPE=$CONFIG
+    cmake --build ./_build/cmake --config $CONFIG --target install
+fi
+
+# do we need to stage?
+if [[ "$STAGE" == "true" ]]
+then
+    mkdir -p $CWD/_install/linux-$(arch)/$CONFIG
+    cp -rf $CWD/src/kit-extension/exts/omni.example.schema $CWD/_install/linux-$(arch)/$CONFIG/
+    mkdir -p $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleSchema
+    mkdir -p $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleCodelessSchema
+    cp -rf $CWD/_install/linux-$(arch)/$CONFIG/omniExampleSchema/OmniExampleSchema/*.* $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleSchema/
+    cp -rf $CWD/_install/linux-$(arch)/$CONFIG/omniExampleSchema/include $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleSchema/
+    cp -rf $CWD/_install/linux-$(arch)/$CONFIG/omniExampleSchema/lib $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleSchema/
+    cp -rf $CWD/_install/linux-$(arch)/$CONFIG/omniExampleSchema/resources $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleSchema/    
+    cp -rf $CWD/_install/linux-$(arch)/$CONFIG/omniExampleCodelessSchema/* $CWD/_install/linux-$(arch)/$CONFIG/omni.example.schema/OmniExampleCodelessSchema/
+fi
+
+# do we need to configure?
+# When using premake, the plugInfo.json files
+# do not get their tokens replaced as premake
+# does not have this functionality built in like cmake
+if [[ "$CONFIGURE" == "true" ]]
+then
+    $CWD/tools/packman/python.sh bootstrap.py usd --configure-plugInfo
+fi
