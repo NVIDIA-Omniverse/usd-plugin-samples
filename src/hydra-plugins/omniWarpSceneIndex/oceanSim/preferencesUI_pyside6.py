98e4e557b2755aa8db47252dd3501bad2110f207
Michael Marks <mmarks@nvidia.com>
2023-08-07 14:20:27 -0400

    Switch to PySide2 generated usdView plugin

--- src/hydra-plugins/omniWarpSceneIndex/oceanSim/preferencesUI_pyside6.py
+++ src/hydra-plugins/omniWarpSceneIndex/oceanSim/preferencesUI_pyside6.py
@@ -3,28 +3,34 @@
 ################################################################################
 ## Form generated from reading UI file 'preferencesUI.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 6.5.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QDoubleSpinBox, QFrame, QHBoxLayout, QLabel,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_Preferences(object):
     def setupUi(self, Ocean_Simulation_Settings):
         if not Ocean_Simulation_Settings.objectName():
             Ocean_Simulation_Settings.setObjectName(u"Ocean_Simulation_Settings")
         Ocean_Simulation_Settings.resize(295, 99)
-        self.verticalLayout = QVBoxLayout(Ocean_Simulation_Settings)
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.prefsOverButtonsLayout = QVBoxLayout()
         self.prefsOverButtonsLayout.setObjectName(u"prefsOverButtonsLayout")
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.scaleLabel = QLabel(Ocean_Simulation_Settings)
+        self.scaleLabel = QLabel()
         self.scaleLabel.setObjectName(u"scaleLabel")
 
         self.horizontalLayout_3.addWidget(self.scaleLabel)
@@ -33,7 +39,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_3.addItem(self.horizontalSpacer_2a)
 
-        self.scaleSpinBox = QDoubleSpinBox(Ocean_Simulation_Settings)
+        self.scaleSpinBox = QDoubleSpinBox()
         self.scaleSpinBox.setObjectName(u"scaleSpinBox")
         self.scaleSpinBox.setDecimals(2)
         self.scaleSpinBox.setMinimum(0.000000000000000)
@@ -46,7 +52,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.directionLabel = QLabel(Ocean_Simulation_Settings)
+        self.directionLabel = QLabel()
         self.directionLabel.setObjectName(u"directionLabel")
 
         self.horizontalLayout_4.addWidget(self.directionLabel)
@@ -55,7 +61,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_4.addItem(self.horizontalSpacer_2b)
 
-        self.directionSpinBox = QDoubleSpinBox(Ocean_Simulation_Settings)
+        self.directionSpinBox = QDoubleSpinBox()
         self.directionSpinBox.setObjectName(u"directionSpinBox")
         self.directionSpinBox.setDecimals(2)
         self.directionSpinBox.setMinimum(0.000000000000000)
@@ -68,7 +74,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_5 = QHBoxLayout()
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.windSpeedLabel = QLabel(Ocean_Simulation_Settings)
+        self.windSpeedLabel = QLabel()
         self.windSpeedLabel.setObjectName(u"windSpeedLabel")
 
         self.horizontalLayout_5.addWidget(self.windSpeedLabel)
@@ -77,7 +83,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_5.addItem(self.horizontalSpacer_2c)
 
-        self.windSpeedSpinBox = QDoubleSpinBox(Ocean_Simulation_Settings)
+        self.windSpeedSpinBox = QDoubleSpinBox()
         self.windSpeedSpinBox.setObjectName(u"windSpeedSpinBox")
         self.windSpeedSpinBox.setDecimals(2)
         self.windSpeedSpinBox.setMinimum(0.000000000000000)
@@ -90,7 +96,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_6 = QHBoxLayout()
         self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
-        self.waterDepthLabel = QLabel(Ocean_Simulation_Settings)
+        self.waterDepthLabel = QLabel()
         self.waterDepthLabel.setObjectName(u"waterDepthLabel")
 
         self.horizontalLayout_6.addWidget(self.waterDepthLabel)
@@ -99,7 +105,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_6.addItem(self.horizontalSpacer_2d)
 
-        self.waterDepthSpinBox = QDoubleSpinBox(Ocean_Simulation_Settings)
+        self.waterDepthSpinBox = QDoubleSpinBox()
         self.waterDepthSpinBox.setObjectName(u"waterDepthSpinBox")
         self.waterDepthSpinBox.setDecimals(2)
         self.waterDepthSpinBox.setMinimum(0.000000000000000)
@@ -112,7 +118,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_7 = QHBoxLayout()
         self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
-        self.waveAmplitudeLabel = QLabel(Ocean_Simulation_Settings)
+        self.waveAmplitudeLabel = QLabel()
         self.waveAmplitudeLabel.setObjectName(u"waveAmplitudeLabel")
 
         self.horizontalLayout_7.addWidget(self.waveAmplitudeLabel)
@@ -121,7 +127,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_7.addItem(self.horizontalSpacer_2e)
 
-        self.waveAmplitudeSpinBox = QDoubleSpinBox(Ocean_Simulation_Settings)
+        self.waveAmplitudeSpinBox = QDoubleSpinBox()
         self.waveAmplitudeSpinBox.setObjectName(u"waveAmplitudeSpinBox")
         self.waveAmplitudeSpinBox.setDecimals(2)
         self.waveAmplitudeSpinBox.setMinimum(0.000000000000000)
@@ -134,7 +140,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_8 = QHBoxLayout()
         self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
-        self.waveDirectionalityLabel = QLabel(Ocean_Simulation_Settings)
+        self.waveDirectionalityLabel = QLabel()
         self.waveDirectionalityLabel.setObjectName(u"waveDirectionalityLabel")
 
         self.horizontalLayout_8.addWidget(self.waveDirectionalityLabel)
@@ -143,7 +149,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_8.addItem(self.horizontalSpacer_2f)
 
-        self.waveDirectionalitySpinBox = QDoubleSpinBox(Ocean_Simulation_Settings)
+        self.waveDirectionalitySpinBox = QDoubleSpinBox()
         self.waveDirectionalitySpinBox.setObjectName(u"waveDirectionalitySpinBox")
         self.waveDirectionalitySpinBox.setMinimum(0.000000000000000)
         self.waveDirectionalitySpinBox.setValue(0.000000000000000)
@@ -157,7 +163,7 @@ class Ui_Preferences(object):
 
         self.prefsOverButtonsLayout.addItem(self.verticalSpacer)
 
-        self.line = QFrame(Ocean_Simulation_Settings)
+        self.line = QFrame()
         self.line.setObjectName(u"line")
         self.line.setFrameShape(QFrame.HLine)
         self.line.setFrameShadow(QFrame.Sunken)
@@ -170,7 +176,7 @@ class Ui_Preferences(object):
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer)
 
-        self.buttonBox = QDialogButtonBox(Ocean_Simulation_Settings)
+        self.buttonBox = QDialogButtonBox()
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setStandardButtons(QDialogButtonBox.Apply|QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
 
